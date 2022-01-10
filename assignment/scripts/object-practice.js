3de2dee7a4d056df68a406db33ec0ea30df44a89
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-01-10 12:31:59 -0600

    Answering questions 1-5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,32 +10,39 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: "Kelsey",
+  lastName: "Peterson",
+  hasSiblings: true,
+  shoeCount: 6,
+  favThreeFoods: ["ramen", "strawberries with yogurt", "veggie burger"]
 };
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
-
-
+let fullName = me.firstName + " " + me.lastName;
+console.log("Test - fullName variable:", fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
-
-
+console.log("Test - first favorite food:", me.favThreeFoods[0]);
+console.log("Test - third favorite food:", me.favThreeFoods[2]);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
-
+console.log("Current shoe count:", me.shoeCount);
+me.shoeCount = me.shoeCount + 1;
+console.log("I got a new pair of shoes! Now my shoe count is:", me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +50,5 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = "green";
+console.log("Test - my favorite color is:", me.favoriteColor);
